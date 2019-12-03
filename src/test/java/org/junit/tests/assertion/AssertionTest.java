90b872719a52282903e387f7181ca92d0beacb10
o.zaher <o.zaher@LAB717P38.laboratori.local>
2019-12-03 10:49:25 +0100

    test tipi primitivi greaterThan

--- src/test/java/org/junit/tests/assertion/AssertionTest.java
+++ src/test/java/org/junit/tests/assertion/AssertionTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertGreaterThan;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
@@ -16,6 +17,7 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Comparator;
 
 import org.junit.Assert;
 import org.junit.ComparisonFailure;
@@ -1001,5 +1003,60 @@ public class AssertionTest {
                 throw t;
             }
         };
+    }
+    
+    @Test
+    public void greaterThan() {
+        Comparator<Integer> comparatorInt = new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                if(o1 == o2) {
+                    return 0;
+                } else if (o1 > o2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+        assertGreaterThan(3,2, comparatorInt);
+        
+        Comparator<Float> comparatorFloat = new Comparator<Float>() {
+            public int compare(Float o1, Float o2) {
+                if(o1 == o2) {
+                    return 0;
+                } else if (o1 > o2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+        assertGreaterThan(3.1f,2.2f, comparatorFloat);
+        
+        Comparator<Double> comparatorDouble = new Comparator<Double>() {
+            public int compare(Double o1, Double o2) {
+                if(o1 == o2) {
+                    return 0;
+                } else if (o1 > o2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+        assertGreaterThan(3.1, 2.2, comparatorDouble);
+        
+        Comparator<Character> comparatorCharacter = new Comparator<Character>() {
+            public int compare(Character o1, Character o2) {
+                if(o1 == o2) {
+                    return 0;
+                } else if (o1 > o2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+        assertGreaterThan('c', 'b', comparatorCharacter);
     }
 }
